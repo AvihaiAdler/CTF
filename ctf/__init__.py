76fd4a7f79d444c2f445051bd1591171866c3094
AvihaiAdler <69742778+AvihaiAdler@users.noreply.github.com>
2020-12-28 20:26:21 +0200

    Update __init__.py

--- ctf/__init__.py
+++ ctf/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from ctf.controller import Songs, Secrets
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '4fb8dac4d9aa0e70d016bccc9a2a17d9c5ff161809f11ecfd79d751b446d0f0c'
+app.config['SECRET_KEY'] = 'secret!'
 data = Songs()
 admin = Secrets()
 
